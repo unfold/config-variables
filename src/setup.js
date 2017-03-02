bc1f4a531f1bbaa759921aec90f61f74d4f1006c
einarlove <post@einarlove.com>
2017-03-02 17:27:18 +0100

    Hotfix add setup.js

---
+++ src/setup.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import {
+  readAppjson,
+  readDotEnv,
+  flattenAppjsonVariables,
+  injectConfig,
+  reportMissingVariables,
+  reportCurrentConfig,
+  getMissingVariables,
+  getConfigKeys,
+} from './helpers'
+
+export default ({
+  appJsonPath = path.join(process.cwd(), 'app.json'),
+  dotEnvPath = path.join(process.cwd(), '.env'),
+  warn = true,
+  verbose = true,
+} = {}) => {
+  const appjson = readAppjson(appJsonPath)
+  const envConfig = readDotEnv(dotEnvPath)
+  const appConfig = flattenAppjsonVariables(appjson)
+
+  const config = { ...appConfig, ...envConfig }
+  injectConfig(config)
+
+  if (warn) {
+    reportMissingVariables(getMissingVariables(appjson))
+  }
+
+  if (verbose) {
+    reportCurrentConfig(getConfigKeys(appjson, envConfig))
+  }
+}
